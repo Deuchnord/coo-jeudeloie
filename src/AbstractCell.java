d43b7b6910e3a95c131206bda1513bfe63ad9b46
Tanghe Jerome <tanghe@a16p2.fil.univ-lille1.fr>
2014-09-17 11:17:33 +0200

    Created class AbstractCell

--- src/AbstractCell.java
+++ src/AbstractCell.java
@@ -1,25 +1,45 @@
 
 public abstract class AbstractCell implements Cell {
 
-	private int index;
-	private Player p;
+	int index;
+	Player player;
 	
-	public AbstractCell(int index) {
-		
-		this.index = index;
-		
-	}
-	
-	public int getIndex() {
-		
-		return this.index;
-		
-	}
-	
+	@Override
 	public boolean canBeLeftNow() {
-		
-		
-		
+		return true;
 	}
-	
+
+	@Override
+	public boolean isRetaining() {
+		return false;
+	}
+
+	@Override
+	public int getIndex() {
+		return this.index;
+	}
+
+	@Override
+	public int handleMove(int diceThrow) {
+		return this.index;
+	}
+
+	@Override
+	public boolean isBusy() {
+		if(player == null)
+			return false;
+		else
+			return true;
+	}
+
+	@Override
+	public Player getPlayer() {
+		return player;
+	}
+
+	@Override
+	public void welcome(Player player) {
+		this.player = player;
+	}
+
 }
