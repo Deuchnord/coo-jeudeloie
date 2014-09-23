0ad95894d6c03b7e92eb78e5976bd66eb42bf5b3
Jérôme Deuchnord <astrofacile@gmail.com>
2014-09-23 18:47:57 +0200

    Finalized Board constructors

--- src/Board.java
+++ src/Board.java
@@ -11,12 +11,22 @@ public class Board {
 	private Cell[] cells;
 	public final int LAST_CELL;
 	
-	
-	public Board()
-	{
-		LAST_CELL = 63;
+	/**
+	 * Default constructor. This creates a classic board.
+	 * Corresponds to calling Board("boards/default").
+	 * @throws IOException appends if the file could not be opened.
+	 */
+	public Board() throws IOException
+	{		
+		this("boards/default");
 	}
-	
+	/**
+	 * Constructs a board with custom cells placement.
+	 * Read the documentation about the board configuration files for more details.
+	 * TODO write documentation about board configuration file
+	 * @param configFile the path to the board configuration file
+	 * @throws IOException appends if the file could not be opened or if the file contains mistakes
+	 */
 	public Board(String configFile) throws IOException {
 		
 		FileInputStream input = new FileInputStream(configFile);
@@ -36,11 +46,39 @@ public class Board {
 				lastCell = Integer.parseInt(command);
 			}
 			
-			if(command.equals("goose")) {
+			else if(command.equals("goose")) {
 				for(int i = 1; i < words.length;i++) {
-					
+					int index = Integer.parseInt(words[i]);
+					cells[index] = new GooseCell(index);
 				}
 			}
+			
+			else if(command.equals("trap")) {
+				for(int i = 1; i < words.length;i++) {
+					int index = Integer.parseInt(words[i]);
+					cells[index] = new TrapCell(index);
+				}
+			}
+			
+			else if(command.equals("wait")) {
+				for(int i = 1; i < words.length;i++) {
+					String[] data = words[i].split(":");
+					int index = Integer.parseInt(data[0]),
+							time = Integer.parseInt(data[1]);
+					cells[index] = new WaitCell(index, time);
+				}
+			}
+			
+			else if(command.equals("teleport")) {
+				for(int i = 1; i < words.length;i++) {
+					String[] data = words[i].split(":");
+					int index = Integer.parseInt(data[0]),
+							dest = Integer.parseInt(data[1]);
+					cells[index] = new TeleportCell(index, dest);
+				}
+			}
+			else
+				throw new IOException("Parse error: unknown key word "+words[0]+" on line "+line);
 			
 		}
 		
